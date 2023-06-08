b554261f7704ebbdd4940d5bf00fb7827a1d4e75
Rahul Jha <rahujha04@gmail.com>
2023-06-08 20:43:51 +0530

    made some changes in js file

--- script.js
+++ script.js
@@ -8,10 +8,12 @@ document.addEventListener('DOMContentLoaded', () => {
     item.addEventListener('touchstart', dragStart, { passive: false });
   });
 
+  let selectedItemId = null;
+
   function dragStart(event) {
     event.preventDefault();
 
-    const itemId = event.target.id;
+    selectedItemId = event.target.id;
 
     if (event.type === 'touchstart') {
       event.target.addEventListener('touchmove', touchMove, { passive: false });
@@ -19,10 +21,8 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
       event.target.addEventListener('mousemove', mouseMove);
       event.target.addEventListener('mouseup', mouseUp);
+      event.target.style.border = '2px dashed #4C43CD'; // Change the border color
     }
-
-    event.dataTransfer.setData('text/plain', itemId);
-    event.target.classList.add('dragging');
   }
 
   function mouseMove(event) {
@@ -35,12 +35,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function mouseUp(event) {
     const itemId = event.target.id;
-    const item = document.getElementById(itemId);
 
-    container2.appendChild(item);
-    item.classList.remove('dragging');
+    if (selectedItemId === itemId) {
+      const item = document.getElementById(itemId);
+      container2.appendChild(item);
+      item.classList.remove('dragging');
+      item.style.border = 'none'; // Remove the border color
 
-    showMessage('Shifting Successful');
+      showMessage('Shifting Successful');
+    }
+
+    selectedItemId = null;
 
     event.target.removeEventListener('mousemove', mouseMove);
     event.target.removeEventListener('mouseup', mouseUp);
@@ -48,12 +53,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function touchEnd(event) {
     const itemId = event.target.id;
-    const item = document.getElementById(itemId);
 
-    container2.appendChild(item);
-    item.classList.remove('dragging');
+    if (selectedItemId === itemId) {
+      const item = document.getElementById(itemId);
+      container2.appendChild(item);
+      item.classList.remove('dragging');
+      item.style.border = 'none'; // Remove the border color
 
-    showMessage('Shifting Successful');
+      showMessage('Shifting Successful');
+    }
+
+    selectedItemId = null;
 
     event.target.removeEventListener('touchmove', touchMove);
     event.target.removeEventListener('touchend', touchEnd);
@@ -76,12 +86,14 @@ document.addEventListener('DOMContentLoaded', () => {
   function resetContainers() {
     items.forEach(item => {
       container1.appendChild(item);
+      item.style.border = 'none'; // Remove the border color
     });
 
     container2.innerHTML = '';
   }
-
   function showMessage(text) {
     alert(text);
   }
+  
+  
 });
